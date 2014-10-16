4553886887407ddb8547c6f66bc0cac744239d58
Tom van Tilburg <tom.van.tilburg@gmail.com>
2014-10-16 15:00:00 +0200

    new layer

--- src/d3.mappu.Layer.js
+++ src/d3.mappu.Layer.js
@@ -3,33 +3,77 @@
 **/
 (function(){
   "use strict";
-/*
-  public vars:
-*/
-    data: [Geojson]     default = null;
-    opacity: int        default = 1;
-    visible: boolean    default =  true;
+  
+  d3.mappu.Layer = function(id, config){
+      this.id = id;
+      this.config = config;
+  };
+  
+  Object.defineProperty(d3.mappu.Layer, 'id', {
+    value: null,
+    get: function() {
+        return this.id;
+    },
+    set: function() {
+        console.warn('can\'t set id afterwards');
+        //not settable
+    }
+  });
+  
+  
+  Object.defineProperty(d3.mappu.Layer, 'data', {
+    value: [],
+    get: function() {
+        return this.data;
+    },
+    set: function(data) {
+        this.data = data;
+        //redraw
+    }
+  });
+  
+  Object.defineProperty(d3.mappu.Layer, 'opacity', {
+    value: 1,
+    get: function() {
+        return this.opacity;
+    },
+    set: function(data) {
+        this.opacity = opacity;
+        //redraw
+    }
+  });
+  
+  Object.defineProperty(d3.mappu.Layer, 'visible', {
+    value: true,
+    get: function() {
+        return this.visible;
+    },
+    set: function(data) {
+        this.visible = visible;
+        //redraw
+    }
+  });
+  
   
   
 /*
     functions exposed:
 */
-//getters/setters
-    .id(int)
-    .data([Geojson])
 
-//singulars  
-    .draw() 
-    .refresh() 
-    
-    
+    //singulars
+    d3.mappu.Layer.prototype.refresh = function(){
+    }
+   
 
 /*    
 functions private:
 */
-    _onAdd(map Object){} //Adds the layer to the given map object 
-    _onRemove(){} //Removes the layer from the map object
+    d3.mappu.Layer.prototype._onAdd = function(map){ //Adds the layer to the given map object
+    }
+    d3.mappu.Layer.prototype._onRemove = function(){ //Removes the layer from the map object
+    }
+    d3.mappu.Layer.prototype._draw = function(){
+    }
     
 
-
-
+});
