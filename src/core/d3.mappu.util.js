d78e9bbca82fd9b20d4a68126dcac84b49c25c7e
Steven M. Ottens <github@minst.net>
2014-10-16 16:18:02 +0200

    removed closure

--- src/core/d3.mappu.util.js
+++ src/core/d3.mappu.util.js
@@ -1,16 +1,11 @@
-﻿(function(){
-    "use strict";
-    d3.mappu.util = {};
-    
-    
-    //create a uniqueID for layers etc.
-    var _ctr = 0;   
-    d3.mappu.util.createID = function(){
-        var id = "ッ-"+_ctr;
-        _ctr++
-        return id;
-    }
+﻿d3.mappu.util = {};
 
-})();
+//create a uniqueID for layers etc.
+var _ctr = 0;   
+d3.mappu.util.createID = function(){
+    var id = "ッ-"+_ctr;
+    _ctr++
+    return id;
+}
 
 //                                                                          マップ
