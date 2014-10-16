a6f41816e28228a55dc10c3ec103913a3aa06792
Tom van Tilburg <tom.van.tilburg@gmail.com>
2014-10-16 13:13:15 +0200

    Added grunt and gitignore settings

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+          options: {
+            // define a string to put between each file in the concatenated output
+            separator: ';'
+          },
+          dist: {
+            // the files to concatenate
+            src: [
+            'src/d3.mappu.js',
+            ],
+            // the location of the resulting JS file
+            dest: 'dist/<%= pkg.name %>.js'
+          }
+        },
+       uglify: {
+          options: {
+            // the banner is inserted at the top of the output
+            banner: '/*! <%= pkg.name %> VERSION: <%= pkg.version %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+          },
+          dist: {
+            files: {
+              'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+            }
+          }
+        },
+        watch: {
+          files: ['src/*.js'],
+          tasks: ['jshint', 'qunit']
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    
+    grunt.registerTask('watch', ['concat','uglify']);
+    grunt.registerTask('default', ['concat','uglify']);
+};
