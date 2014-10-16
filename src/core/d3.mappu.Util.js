04037e96397255c44a2e5161534e6db0fc18bd1f
Steven M. Ottens <github@minst.net>
2014-10-16 15:23:54 +0200

    created an mappu UID generator

---
+++ src/core/d3.mappu.Util.js
@@ -0,0 +1,17 @@
+﻿import "../d3.mappu";
+
+(function(){
+    "use strict";
+    
+    var _ctr = 0;
+    
+    
+    d3.mappu.createID = function(){
+        var id = "MAPPU-"+_ctr;
+        _ctr++
+        return id;
+    }
+
+})
+
+//                                                                          マップ
