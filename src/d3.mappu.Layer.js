3135f97aa00c92f7070321ad27ad4c977ee76b56
Tom van Tilburg <tom.van.tilburg@gmail.com>
2014-10-16 16:23:44 +0200

    New vectorlayer Added test

--- src/d3.mappu.Layer.js
+++ src/d3.mappu.Layer.js
@@ -2,86 +2,36 @@
  Generic layer object, to be extended.
 **/
 
-(function(){
-  "use strict";
-  d3.mappu.Layer = function(name, config) {
+
+"use strict";
+d3.mappu.Layer = function(name, config) {
     return d3_mappu_Layer(name, config);
-  };
+};
+
+d3_mappu_Layer = function(name, config){
+    var layer = {};
+    this.config = config;
+    layer.name = name;
+    layer.id = null;//TODO: automatic ID gen
+    layer.name = name;
+    layer.opacity = 1;
+    layer.visible = true;  
   
-  d3_mappu_Layer = function(name, config){
-      var layer = {};  
-      this.name = name;
-      this.config = config;
-      this.id = null;//TODO: automatic ID gen
-      
-      
-      Object.defineProperty(layer, 'name', {
-        value: '',
-        get: function() {
-            return this.name;
-        },
-        set: function(name) {
-            this.name = name;
-        }
-      });
-      
-      //TODO: Move to vectorlayer
-      Object.defineProperty(layer, 'features', {
-        value: [],
-        get: function() {
-            return this.features;
-        },
-        set: function(data) {
-            this.features = features;
-            //redraw
-        }
-      });
-      
-      Object.defineProperty(layer, 'opacity', {
-        value: 1,
-        get: function() {
-            return this.opacity;
-        },
-        set: function(data) {
-            this.opacity = opacity;
-            //redraw
-        }
-      });
-      
-      Object.defineProperty(layer, 'visible', {
-        value: true,
-        get: function() {
-            return this.visible;
-        },
-        set: function(data) {
-            this.visible = visible;
-            //redraw
-        }
-      });
-      
-      
-      
-        /*
-         exposed:
-        */
-    
-        layer.refresh =  function(){
-        };
-        layer.moveUp = function(){
-        };
-        layer.moveDown = function(){
-        };
-    
-        /*    
-         private:
-        */
-        this._onAdd =  function(map){ //Adds the layer to the given map object
-        };
-        this._onRemove = function(){ //Removes the layer from the map object
-        };
-        this._draw = function(){
-        };
-        
-        return layer;
+    /* exposed: */
+    layer.refresh =  function(){
     };
-})();
+    layer.moveUp = function(){
+    };
+    layer.moveDown = function(){
+    };
+
+    /* private: */
+    this._onAdd =  function(map){ //Adds the layer to the given map object
+    };
+    this._onRemove = function(){ //Removes the layer from the map object
+    };
+    this._draw = function(){
+    };
+    
+    return layer;
+};
