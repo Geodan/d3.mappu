c1a7f9095f42f20038691f18c849ec4314987b83
Steven M. Ottens <github@minst.net>
2014-10-21 17:32:30 +0200

    Moved a bit of Toms example to actual code

--- src/layer/d3.mappu.VectorLayer.js
+++ src/layer/d3.mappu.VectorLayer.js
@@ -6,11 +6,15 @@
   };
   
   d3_mappu_VectorLayer = function(name, config) {
-   
+	  var self = this;
       d3_mappu_Layer.call(this,name, config);
       var layer = d3_mappu_Layer(name, config);
       this._layertype = 'vector';
+	  this.path = d3.geo.path()
+					.projection(map.projection);
       var _data = [];
+	  this.drawboard = map.svg.append('path');
+	  
       /* exposed */
       Object.defineProperty(layer, 'data', {
         get: function() {
@@ -23,9 +27,9 @@
       });
       
       layer.draw = function(){
-          this.drawboard
-            .datum(this.data)
-            .attr("d", this.map.path);
+          self.drawboard
+            .datum(layer.data)
+            .attr("d", self.path);
       };
       
       return layer;
