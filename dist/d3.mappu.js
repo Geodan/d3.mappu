b6a55b1fe679df6023f69a316360ea5b829de31e
Tom van Tilburg <tom.van.tilburg@gmail.com>
2014-11-03 17:16:02 +0100

    Added wms option for rasterlayer

--- dist/d3.mappu.js
+++ dist/d3.mappu.js
@@ -256,6 +256,7 @@ d3_mappu_Layer = function(name, config){
         _map = map;
         layer.drawboard = _map.svg.append('g');
         _map.addLayer(layer);
+        layer.draw();
         return layer;
     };
     
@@ -398,6 +399,8 @@ d3_mappu_Layer = function(name, config){
       var layertype = 'raster';
       var drawboard;
       var _url = config.url;
+      var _ogc_type = config.ogc_type || 'tms';
+      var _layers = config.layers;
       
       Object.defineProperty(layer, 'url', {
         get: function() {
@@ -409,9 +412,48 @@ d3_mappu_Layer = function(name, config){
         }
       });
       
+      Object.defineProperty(layer, 'layers', {
+        get: function() {
+            return _layers;
+        },
+        set: function(val) {
+            _layers = val;
+            draw();
+        }
+      });
+      
       
       //Clear all tiles
       layer.clear = function(){
+      };
+      
+      var tileurl = function(d){
+          var url;
+          if (_ogc_type == 'tms') {
+              url = _url    
+                    .replace('{s}',["a", "b", "c", "d"][Math.random() * 4 | 0])
+                    .replace('{z}',d[2])
+                    .replace('{x}',d[0])
+                    .replace('{y}',d[1])
+                    //FIXME: why are these curly brackets killed when used with polymer?
+                    .replace('%7Bs%7D',["a", "b", "c", "d"][Math.random() * 4 | 0])
+                    .replace('%7Bz%7D',d[2])
+                    .replace('%7Bx%7D',d[0])
+                    .replace('%7By%7D',d[1]);
+          }
+          else if (_ogc_type == 'wms'){
+                //This calculation only works for tiles that are square and always the same size
+                var numtiles = 2 << (d[2]-1);
+                var tilesize = (20037508.34 * 2) / (numtiles);
+                var x = -20037508.34 + (d[0] * tilesize);
+                var y = 20037508.34 - ((d[1]+1) * tilesize);//shift 1 down, because we want LOWER left
+                var bbox = x + ", "+ y + "," + (x + tilesize) + "," + (y + tilesize);
+                url =  _url + 
+                     "&bbox=" + bbox + 
+                     "&layers=" + _layers + 
+                     "&service=WMS&version=1.1.0&request=GetMap&tiled=true&styles=&width=256&height=256&srs=EPSG:900913&transparent=TRUE&format=image%2Fpng";
+          }
+          return url;
       };
       
       //Draw the tiles (based on data-update)
@@ -424,20 +466,7 @@ d3_mappu_Layer = function(name, config){
          var imageEnter = image.enter();
          imageEnter.append("image")
               .classed('tile',true)
-              .attr("xlink:href", function(d) {
-                var url = "";
-                url = _url    
-                    .replace('{s}',["a", "b", "c", "d"][Math.random() * 4 | 0])
-                    .replace('{z}',d[2])
-                    .replace('{x}',d[0])
-                    .replace('{y}',d[1])
-                    //FIXME: why are these curly brackets killed when used with polymer?
-                    .replace('%7Bs%7D',["a", "b", "c", "d"][Math.random() * 4 | 0])
-                    .replace('%7Bz%7D',d[2])
-                    .replace('%7Bx%7D',d[0])
-                    .replace('%7By%7D',d[1]);
-                return url;
-              })
+              .attr("xlink:href", tileurl)
               .attr("width", 1)
               .attr("height", 1)
               .attr('opacity', self._opacity)
--- src/layer/d3.mappu.Layer.js
+++ src/layer/d3.mappu.Layer.js
@@ -27,6 +27,7 @@ d3_mappu_Layer = function(name, config){
         _map = map;
         layer.drawboard = _map.svg.append('g');
         _map.addLayer(layer);
+        layer.draw();
         return layer;
     };
     
