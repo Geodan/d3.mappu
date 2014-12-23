288d7d611e4c676d922bfa99ae789632259a1bb3
Tom van Tilburg <tom.van.tilburg@gmail.com>
2014-12-23 16:46:19 +0100

    Better zoomlevel

--- src/map/d3.mappu.Map.js
+++ src/map/d3.mappu.Map.js
@@ -54,7 +54,7 @@ d3_mappu_Map = function(id, config) {
 	
 	var _center = config.center || [0,0];
 	var _projection = config.projection || d3.geo.mercator();
-	var _zoom = config.zoom || 10;
+	var _zoom = config.zoom || 22;
 	var _maxZoom = config.maxZoom || 24;
 	var _minZoom = config.minZoom || 15;
 	var _maxView = config.maxView || [[-180,90],[180,-90]];
@@ -167,15 +167,19 @@ d3_mappu_Map = function(id, config) {
             return _zoom;
         },
         set: function(val) {
-        	var self = this;
-        	_zoom = val;
-        	_zoombehaviour.scale((1 << val) / 2 / Math.PI);
-        	//Adapt projection based on new zoomlevel
-			_projection
-			   .scale(_zoombehaviour.scale() / 2 / Math.PI)
-			   .translate(_zoombehaviour.translate());
-			//recenter map based on new zoomlevel
-        	this.center = _center;
+        	if (val <= _maxZoom && val >= _minZoom){
+				_zoom = val;
+				_zoombehaviour.scale((1 << val) / 2 / Math.PI);
+				//Adapt projection based on new zoomlevel
+				_projection
+				   .scale(_zoombehaviour.scale() / 2 / Math.PI)
+				   .translate(_zoombehaviour.translate());
+				//recenter map based on new zoomlevel
+				this.center = _center;
+			}
+			else {
+				console.log('Zoomlevel exceeded', val);
+			}
         }
     });
 
@@ -186,6 +190,7 @@ d3_mappu_Map = function(id, config) {
         },
         set: function(val) {
             _minZoom = val;
+            this.zoombehaviour.scaleExtent([1 << _minZoom, 1 << _maxZoom]);
         }
     });
 // .maxZoom : (zoomlevel)
@@ -195,6 +200,7 @@ d3_mappu_Map = function(id, config) {
         },
         set: function(val) {
             _maxZoom = val;
+            this.zoombehaviour.scaleExtent([1 << _minZoom, 1 << _maxZoom]);
         }
     });
 // .maxView : ([[long,lat],[long,lat]])
