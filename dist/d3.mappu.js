11183da6d804d0d6af2f7222dba3116c292e1b5d
Rubio Vaughan <rubio.vaughan@geodan.nl>
2016-04-12 15:22:54 +0200

    grunt grunt

--- dist/d3.mappu.js
+++ dist/d3.mappu.js
@@ -1059,9 +1059,13 @@ d3_mappu_Layer = function(name, config){
       	  if (d.geometry.type == 'Point' && d.style && d.style['iconimg']){
       	  	  var x = project(d.geometry.coordinates)[0];
               var y = project(d.geometry.coordinates)[1];
+              var width = d.style && d.style.width ? d.style.width : 
+                           (style.width ? style.width : 32);
+              var height = d.style && d.style.height ? d.style.height : 
+                           (style.height ? style.height : 37);
               var img = d3.select(this).append("image")
-              	.attr("width", 32)
-                .attr("height", 37)
+              	.attr("width", width)
+                .attr("height", height)
                 .attr("xlink:href", function(d){
 					return 'data:image/' + d.style['iconimg_encoding'] +','+ d.style['iconimg_bytearray'];
 				});
@@ -1069,9 +1073,13 @@ d3_mappu_Layer = function(name, config){
       	  if (d.geometry.type == 'Point' && d.style && d.style['marker-url']){
       	  	  var x = project(d.geometry.coordinates)[0];
               var y = project(d.geometry.coordinates)[1];
+              var width = d.style && d.style.width ? d.style.width : 
+                           (style.width ? style.width : 32);
+              var height = d.style && d.style.height ? d.style.height : 
+                           (style.height ? style.height : 37);
               var img = d3.select(this).append('g').append("image")
-              	.attr("width", 32)
-                .attr("height", 37)
+              	.attr("width", width)
+                .attr("height", height)
                 .style('pointer-events','visiblepainted')//make clickable
               	//.attr("x",x-12.5) //No need setting x and y, since it's reset later
 				//.attr("y",y-25)
@@ -1163,8 +1171,10 @@ d3_mappu_Layer = function(name, config){
 				  entities.select('path').transition().duration(duration).attr("d", _path);
 				  entities.select('image').transition().duration(duration).each(function(d){
                     //TODO: create something nice customizable for widh-height calculations
-				  	var width =  32; //calcwidth(layer.map.zoom);
-				  	var height = 37; //calcheight(layer.map.zoom);
+                    var width = d.style && d.style.width ? d.style.width : 
+                                 (style.width ? style.width : 32); //calcwidth(layer.map.zoom);
+                    var height = d.style && d.style.height ? d.style.height : 
+                                 (style.height ? style.height : 37); //calcheight(layer.map.zoom);
 				  	var x = project(d.geometry.coordinates)[0];
 				  	var y = project(d.geometry.coordinates)[1];
 				  	var offsetx = width/2;
