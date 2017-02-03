109d4a6b8a2df00628a57f88b885e2befcf9c102
Tom van Tilburg <tom.van.tilburg@gmail.com>
2017-02-03 15:49:57 +0100

    vector tiles back to working version

--- dist/d3.mappu.js
+++ dist/d3.mappu.js
@@ -1272,6 +1272,7 @@ d3_mappu_Layer = function(name, config){
       var _path;
       var _projection;
       var tms = config.tms;
+      var layername = config.layername;
       var style = config.style || {};
       var labelStyle = config.labelStyle || {};
       var _events = config.events;
@@ -1340,52 +1341,61 @@ d3_mappu_Layer = function(name, config){
 		
 		this._xhr = d3.json(url, function(error, json) {
 			if (error) throw error;
-			if (json.objects.pand){
-				var k = Math.pow(2, d[2]) * 256; // size of the world in pixels
-				
-				_path.projection()
-					.translate([k / 2 - d[0] *256, k / 2 - d[1] *256]) // [0�,0�] in pixels
-					.scale(k / 2 / Math.PI);
-				
-				function matrix(a, b, c, d, tx, ty) {
-				  return d3.geoTransform({
-					point: function(x, y) {
-					  this.stream.point(a * x + b * y + tx, c * x + d * y + ty);
-					}
-				  });
+			
+			var k = Math.pow(2, d[2]) * 256; // size of the world in pixels
+			
+			_path.projection()
+				.translate([k / 2 - d[0] *256, k / 2 - d[1] *256]) // [0�,0�] in pixels
+				.scale(k / 2 / Math.PI);
+			/* TT: WORK IN PROGRESS FOR ALREADY PROJECTED DATA
+			function matrix(a, b, c, d, tx, ty) {
+			  return d3.geoTransform({
+				point: function(x, y) {
+				  this.stream.point(a * x + b * y + tx, c * x + d * y + ty);
 				}
-				var tx = 0; //k / 2 - d[0] *256;
-				var ty = 0 ; //k / 2 - d[0] *256;
-				
-				var scale = 0.00390625;
-				var path = d3.geoPath()
-					.projection(matrix(scale, 0, 0, scale, tx, ty));
-					
-				//var features = json.features;
-				var collection = topojson.feature(json, json.objects.pand);
-				
-				var entities = tile.selectAll('path').data(collection.features, function(d){
-					return d.id;
-				});
-				var newentity = entities.enter().append('path')
-					.attr('id',function(d){
-							return 'entity'+ d.id;
-					})
-					.attr('class',function(d){return d.properties.kind;})
-					.attr("d", path)
-					.style('pointer-events','visiblepainted');//make clickable;
-				tile.append('circle').attr('cx',50).attr('cy',50).attr('r',10).style('fill','red');
-				entities.exit().remove();
-				
-				// Add events from config
-				  if (_events){
-					  _events.forEach(function(d){
-						 newentity.each(function(){
-							d3.select(this).select('path').on(d.event, d.action);
-						 });
-					  });
-				  }
+			  });
 			}
+			var tx = 0; //k / 2 - d[0] *256;
+			var ty = 0 ; //k / 2 - d[0] *256;
+			
+			
+			var scale = 1/256;
+			var path = d3.geoPath()
+				.projection(matrix(scale, 0, 0, scale, tx, ty));
+			/* END OF WORK IN PROGRESS */
+			
+			if (json.objects){
+				var features = topojson.feature(json,json.objects[layername]).features;	
+			} else if (jsonl.features){
+				var features = json.features;
+			} else {
+				throw "Can't work with this vectortile data";
+			}
+			
+			//TT: TODO: add option for topojson input
+			//var collection = topojson.feature(json, json.objects.pand);
+			
+			var entities = tile.selectAll('path').data(features, function(d){
+				return d.id;
+			});
+			var newentity = entities.enter().append('path')
+				.attr('id',function(d){
+						return 'entity'+ d.id;
+				})
+				.attr('class',function(d){return d.properties.kind;})
+				.attr("d", _path)
+				.style('pointer-events','visiblepainted');//make clickable;
+			
+			entities.exit().remove();
+			
+			// Add events from config
+			  if (_events){
+				  _events.forEach(function(d){
+					 newentity.each(function(){
+						d3.select(this).select('path').on(d.event, d.action);
+					 });
+				  });
+			  }
 		});
 		
       }
@@ -1892,6 +1902,7 @@ d3_mappu_Layer = function(name, config){
          image.exit()
          	.attr('src',null)
          	.remove();
+
       };
 
       var refresh = function(){
