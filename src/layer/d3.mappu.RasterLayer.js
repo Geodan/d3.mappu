3e8b4337f575f143cc5f2d21a8c65fadb647603d
Tom van Tilburg <tom.van.tilburg@gmail.com>
2017-08-22 16:59:19 +0200

    updated rasterlayer to new d3-tile

--- src/layer/d3.mappu.RasterLayer.js
+++ src/layer/d3.mappu.RasterLayer.js
@@ -95,7 +95,7 @@
           	}
           	url = _url +
           		"&layer=" + _layers +
-          		"&SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&STYLE=default&TILEMATRIXSET=nltilingschema&TILEMATRIX="+d[2]+ "&TILEROW="+d[1]+"&TILECOL="+d[0]+"&FORMAT=image%2Fpng";
+          		"&SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&STYLE=default&TILEMATRIXSET=nltilingschema&TILEMATRIX="+d.z+ "&TILEROW="+d.y+"&TILECOL="+d.x+"&FORMAT=image%2Fpng";
       	  }
           else if (_ogc_type == 'wms'){
           	if (_url.indexOf('?') < 0){
@@ -214,8 +214,8 @@
               .attr("width", 1)
               .attr("height", 1)
               .attr('opacity', this.opacity)
-              .attr("x", function(d) { return d[0]; })
-              .attr("y", function(d) { return d[1]; })
+              .attr("x", function(d) { return d.tx; })
+              .attr("y", function(d) { return d.ty; })
               //TODO: working on this
               //.on('click', getFeatureInfo);
          }
